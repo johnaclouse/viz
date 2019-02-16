655d4c8e54d1b639ea14331bda737a90bcc831c3
johnaclouse <johnaclouse@gmail.com>
2019-02-16 00:10:15 -0800

    Initial commit.

---
+++ R/plot_ggpairs.R
@@ -0,0 +1,39 @@
+#' Helper function for ggally::ggpairs
+#'
+#' @name plot_ggpairs
+#' @import tidyverse
+#' @import ggplot2
+#' @import GGally
+#' @import rlang
+#' @importFrom magrittr "%>%"
+#' @param data data frame containing features and target
+#' @param color column name to use for color aesthetic in pair plots
+#' @param density_alpha alpha setting for frequency density plots
+#' 
+#' @return ggplot object containing pairwise comparisons
+#' @export
+#'
+#' @examples
+#' plot_ggpairs(iris)
+
+plot_ggpairs <- function(data,
+                         color = NULL,
+                         density_alpha = 0.5) {
+  color_expr <- enquo(color)
+  if (rlang::quo_is_null(color_expr)) {
+    g <- data %>%
+      ggpairs(lower = "blank")
+  } else {
+    color_name <- quo_name(color_expr)
+    g <- data %>%
+      ggpairs(
+        mapping = aes_string(color = color_name),
+        lower = "blank",
+        legend = 1,
+        diag = list(continuous = wrap("densityDiag",
+                                      alpha = density_alpha))
+      ) +
+      theme(legend.position = "bottom")
+  }
+  return(g)
+}
